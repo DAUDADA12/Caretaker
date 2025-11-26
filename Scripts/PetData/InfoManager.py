5880dd05ddc5eddf8e8a34c87415ad63179eccab
DAUDADA12 <atharvdev@hotmail.com>
2025-11-26 08:33:41 +0400

    Update 1

--- Scripts/PetData/InfoManager.py
+++ Scripts/PetData/InfoManager.py
@@ -1,6 +1,7 @@
 from py4godot.classes import gdclass
 from py4godot.classes.Node2D import Node2D
 from py4godot.classes.core import NodePath
+from py4godot import private
 
 
 @gdclass
@@ -9,3 +10,7 @@ class InfoManager(Node2D):
 	PetName: str = ""
 	PetAge: int = 0
 	PetType: str = ""
+	PetHappines: int = 0
+	PetCleanlines: int = 100
+	PetHunger: int = 0
+	PetEnergy: int = 0
